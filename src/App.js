0295b45964adbdf5959060bdc38947c1c4887f0e
khalil.azaez <khalilazaez@gmail.com>
2022-12-24 00:05:41 +0100

    adding style to noteHistory

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import Note from './Note/Note';
 import './App.css';
 import React , { Component } from 'react';
+import Hisnote from './HisNotes/Hisnote';
 
 class  App extends Component {
+
+
+  state = {
+    notes: [
+      {title: 'hello' , note: 'm tryinngggg'}
+    ]
+  }
+
+
+
+
+  onclickhandler = () => {
+
+      
+  }
 
 
   render () {
     return (
       <div className="App">
         <h1>Notes</h1>
-        <Note/>
+        <Note click={this.onclickhandler}/>
+        <Hisnote title={this.state.notes[0].title}  note={this.state.notes[0].note} />
       </div>
     );
   }
--- src/HisNotes/Hisnote.js
+++ src/HisNotes/Hisnote.js
@@ -1,9 +1,17 @@
+import './Hisnotes.css'
+
 const Hisnote = props => {
 
+
+    const style = {
+        color : 'white'
+    }
+
         return (
-            <div>
+            <div className="hisnote">
                 <h1>{props.title}</h1>
                 <p>{props.note}</p>
+                <button>X</button>
             </div>
 
         )
